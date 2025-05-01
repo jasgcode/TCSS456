89185fdf22ffa5cd96921d90cb31e874922caee9
johnnyas <johnasgarnica@gmail.com>
2025-05-01 10:09:13 -0700

    update files

---
+++ app/index.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+
+input_vector1 = [1.66, 1.56]
+weight_1 = [1.45, -0.66]
+
+dot_product = np.dot(input_vector1, weight_1)
+print("Dot product of input_vector1 and weight_1:", dot_product)
